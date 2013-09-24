9aa52ef39b3a346ea85264729db0545f3f39de64
Madsss11 <madsss11@student.uia.no>
2013-09-24 19:10:07 +0200

    opg5.py

---
+++ opg5.py
@@ -0,0 +1,17 @@
+name = 'Mads Soeftestad'
+age = 21
+height = 186 
+weight = 92
+eyes = 'blue'
+teeth = 'white'
+hair = 'Blonde'
+
+print "Let's talk about %s." % name
+print "He's %d centimeters tall." % height
+print "He's %d kilos." %weight
+print "Beautiful guy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually white %s depending on the coffe and snus." % teeth
+
+print "If i add %d, %d, and %d I get %d." % (
+age, height, weight, age + height + weight)
