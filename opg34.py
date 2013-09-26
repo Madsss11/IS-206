8d0e92ddd196a043aed2cf4111552bf8034f0743
Madsss11 <madsss11@student.uia.no>
2013-09-26 10:00:15 +0200

    opg34.py

---
+++ opg34.py
@@ -0,0 +1,11 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+
+#The animal at 1. is the python
+#The third animal is at 2 and is the peacock
+#The first animal is at 0 and is the bear
+#The animal at 3. is the kangaroo
+#The fifth animal is at 4 and is the whale
+#The animal at 2. is the Peacock
+#The sixth animal is at 5 and is the platypus
+#The animal at 4. is the whale
