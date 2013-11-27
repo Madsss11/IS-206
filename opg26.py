d9d7b464e41d7d28559c90329c93124598e4af61
Madsss11 <madsss11@student.uia.no>
2013-11-27 13:24:01 +0100

    OPG26FINAL.py

--- opg26.py
+++ opg26.py
@@ -1,4 +1,4 @@
-def break_words(stuff):
+﻿def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
     return words
@@ -7,14 +7,14 @@ def sort_words(words):
     """Sorts the words."""
     return sorted(words)
 
-def print_first_word(words):
+def print_first_word(words): # la til et kolon
     """Prints the first word after popping it off."""
     word = words.pop(0)
     print word
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1)
+    word = words.pop(-1) # la til et parantes på slutten
     print word
 
 def sort_sentence(sentence):
@@ -38,26 +38,26 @@ def print_first_and_last_sorted(sentence):
 print "Let's practice everything."
 print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
 
-poem = """
+poem = """ 
 \tThe lovely world
-with logic so firmly planted
-cannot discern \n the needs of love
-nor comprehend passion from intuition
-and requires an explantion
-\n\t\twhere there is none.
-"""
+\twith logic so firmly planted
+\tcannot discern the needs of love
+\tnor comprehend passion from intuition
+\tand requires an explantion
+\tthere there is none.
+""" #laget nytt oppsett for diktet
 
 
 print "--------------"
 print poem
 print "--------------"
 
-five = 10 - 2 + 3 - 5
+five = 10 - 3 + 3 - 5 # rettet matematisk feil
 print "This should be five: %s" % five
 
 def secret_formula(started):
     jelly_beans = started * 500
-    jars = jelly_beans / 1000
+    jars = jelly_beans / 1000 #rettet på skråstreken som sto feil vei
     crates = jars / 100
     return jelly_beans, jars, crates
 
@@ -71,17 +71,17 @@ print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point) #skrivefeil og satt inn parantes på slutten
 
 
-sentence = "All good t\ things come to those who weight."
+sentence = "All good things come to those who wait." #skrivefeil og fjernet tabben siden det ble rotete
 
 words = break_words(sentence)
 sorted_words = sort_words(words)
 
 print_first_word(words)
-print_last_word(words)
-print_first_word(sorted_words)
+print_last_word(words) #
+print_first_word(sorted_words) #fjernet punktum på begynnelsen
 print_last_word(sorted_words)
 sorted_words = sort_sentence(sentence)
 print sorted_words
